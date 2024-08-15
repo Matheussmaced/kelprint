53bf45d1f3d1d2da54131091d16cf1931e1b4833
Matheussmaced <mateusbyakuya3@gmail.com>
2024-08-15 12:02:56 -0300

    feat: logic login

--- src/components/login/index.tsx
+++ src/components/login/index.tsx
@@ -3,17 +3,31 @@ import { defaultTheme } from "@/themes/default";
 import { ThemeProvider } from "styled-components";
 import { ButtonsContainer, FormContainer, LinkContainer, Main, Submit} from "./styles";
 import { LogIn } from "lucide-react";
+import { v4 as uuidv4 } from "uuid";
 
 export default function Login() {
-  const login = "admin"
-  const password = "admin"
 
-  
+  const logIn = (e:any) => {
+    e.preventDefault();
+
+    const login:string = e.target.login.value;
+    const password:string  = e.target.password.value;
+
+    if(login == "admin" && password == "admin"){
+        const token = uuidv4();
+
+        localStorage.setItem("TOKEN_FRONT", token)
+        window.location.href = "/customer-registration"
+    }else(
+      console.log("Login ou senha inválidas ")
+    )
+  }
+
   return(
     <ThemeProvider theme={defaultTheme}>
       <GlobalStyles />
     <Main>
-      <FormContainer>
+      <FormContainer onSubmit={(e) => logIn(e)}>
         <p>Entrar no sistema</p>
         <form>
           <div>
