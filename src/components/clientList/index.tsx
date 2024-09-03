5524776d752103d05f2ea85940cd33315bdf4e60
Matheussmaced <mateusbyakuya3@gmail.com>
2024-09-03 08:38:02 -0300

    fix: return

--- src/components/clientList/index.tsx
+++ src/components/clientList/index.tsx
@@ -1,17 +1,9 @@
 import { GlobalStyles } from "@/styles/global";
 import { defaultTheme } from "@/themes/default";
-import { Loader, Pen, TrafficCone, Trash2 } from "lucide-react";
+import { Loader, Pen, Trash2 } from "lucide-react";
 import Link from "next/link";
 import { ThemeProvider } from "styled-components";
-import {
-  ButtonsContainer,
-  InformationContainerMaster,
-  LinkContainer,
-  Loading,
-  Main,
-  MainFinishedFalse,
-  MainFinishedDanger // Importar o novo componente estilizado
-} from "./styles";
+import { ButtonsContainer, InformationContainerMaster, LinkContainer, Loading, Main, MainFinishedFalse } from "./styles";
 import axios from "axios";
 import { BACKEND_URL } from "@/api";
 import { useEffect, useState } from "react";
@@ -28,98 +20,98 @@ interface ClientInfoProps {
   }[];
 }
 
-export default function ClientList({ clients: initialClient }: ClientInfoProps) {
+export default function ClientList({clients:initialClient}:ClientInfoProps){
+
   const [client, setClient] = useState(initialClient);
   const [clientId, setClientId] = useState("");
-  const [dangerClientId, setDangerClientId] = useState(""); // Estado para armazenar o clientId em perigo
 
   useEffect(() => {
     setClient(initialClient);
-  }, [initialClient]);
+  },[initialClient])
 
   const formatClientId = (id: string) => {
     return id.length > 4 ? `...${id.slice(-4)}` : id;
-  };
+  }
 
   const deleteClient = (id: string) => {
+
     axios.delete(`${BACKEND_URL}/${id}`);
-    setClient(client.filter((clients) => clients.clientId !== id));
-  };
+
+    setClient(client.filter(clients => clients.clientId !== id))
+  }
 
   const editClient = (id: string) => {
     setClientId(id);
-  };
+  }
 
-  const toggleDanger = (id: string) => {
-    // Alterna o estado de perigo para o clientId específico
-    if (dangerClientId === id) {
-      setDangerClientId(""); // Se já estiver em perigo, desativa
-    } else {
-      setDangerClientId(id); // Ativa o perigo para o clientId clicado
-    }
-  };
-
-  if (!client || client.length === 0) {
-    return (
+  if (!client ||  client.length === 0) {
+    return(
       <ThemeProvider theme={defaultTheme}>
         <Loading>
           <Loader width={25} />
           <span>Carregando clientes...</span>
         </Loading>
       </ThemeProvider>
-    );
+    )
   }
-
+  
   return (
     <ThemeProvider theme={defaultTheme}>
       <GlobalStyles />
       <>
-        {client.map((client, index) => {
-          // Verifica se há pedidos não finalizados
-          const hasUnfinishedOrders = client.clientOrders.some(
-            (order) => !order.finished
-          );
+      {client.map((client, index) => {
+          if ((client.clientOrders.map((finish) => finish.finished).includes(false))) {
+            return (
+              <MainFinishedFalse key={client.clientId}>
+                <InformationContainerMaster>
+                  <span>{index + 1}</span>
+                  <LinkContainer href={`/customer-registration/client/${client.clientId}`}>
+                    <span>{client.clientName}</span>
+                  </LinkContainer>
+                  <span>{client.clientNumber}</span>
+                  <span>{formatClientId(client.clientId)}</span>
 
-          // Determina o componente a ser renderizado com base no estado de perigo
-          let ComponentToRender;
-          if (dangerClientId === client.clientId) {
-            ComponentToRender = MainFinishedDanger;
-          } else if (hasUnfinishedOrders) {
-            ComponentToRender = MainFinishedFalse;
-          } else {
-            ComponentToRender = Main;
-          }
-
-          return (
-            <ComponentToRender key={client.clientId}>
-              <InformationContainerMaster>
-                <span>{index + 1}</span>
-                <LinkContainer
-                  href={`/customer-registration/client/${client.clientId}`}
-                >
-                  <span>{client.clientName}</span>
-                </LinkContainer>
-                <span>{client.clientNumber}</span>
-                <span>{formatClientId(client.clientId)}</span>
-
-                <ButtonsContainer>
-                  <button onClick={() => toggleDanger(client.clientId)}>
-                    <TrafficCone size={18} />
-                  </button>
-                  <Link href={`/customer-registration/editClient/${client.clientId}`}>
-                    <button>
-                      <Pen size={16} onClick={() => editClient(client.clientId)} />
+                  <ButtonsContainer>
+                    <Link href={`/customer-registration/editClient/${client.clientId}`}>
+                      <button>
+                        <Pen size={16} onClick={() => editClient(client.clientId)} />
+                      </button>
+                    </Link>
+                    <button onClick={() => deleteClient(client.clientId)}>
+                      <Trash2 size={16} />
                     </button>
-                  </Link>
-                  <button onClick={() => deleteClient(client.clientId)}>
-                    <Trash2 size={16} />
-                  </button>
-                </ButtonsContainer>
-              </InformationContainerMaster>
-            </ComponentToRender>
-          );
+                  </ButtonsContainer>
+                </InformationContainerMaster>
+              </MainFinishedFalse>
+            );
+          } else {
+            return (
+              <Main key={client.clientId}>
+                <InformationContainerMaster>
+                  <span>{index + 1}</span>
+                  <LinkContainer href={`/customer-registration/client/${client.clientId}`}>
+                    <span>{client.clientName}</span>
+                  </LinkContainer>
+                  <span>{client.clientNumber}</span>
+                  <span>{formatClientId(client.clientId)}</span>
+  
+                  <ButtonsContainer>
+                    <Link href={`/customer-registration/editClient/${client.clientId}`}>
+                      <button>
+                        <Pen size={16} onClick={() => editClient(client.clientId)} />
+                      </button>
+                    </Link>
+                    <button onClick={() => deleteClient(client.clientId)}>
+                      <Trash2 size={16} />
+                    </button>
+                  </ButtonsContainer>
+                </InformationContainerMaster>
+              </Main>
+            );
+          }
+          
         })}
       </>
     </ThemeProvider>
-  );
+  )
 }
